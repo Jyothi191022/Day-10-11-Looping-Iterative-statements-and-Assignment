8d7ef0d58548b4cad1cb6b8d16f3af30a8f4538a
Jyothi Ganapathiraju <96363260+Jyothi191022@users.noreply.github.com>
2025-10-24 22:23:15 +0530

    Sum of Digits

---
+++ SumOfDigits.java
@@ -0,0 +1,21 @@
+package com.codegnan.controlstatements;
+
+import java.util.Scanner;
+
+public class SumOfDigits {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int num = scanner.nextInt();
+		int sum = 0;
+		int temp = num;
+		while (temp != 0) {
+			int digit = temp % 10;
+			sum += digit;
+			temp = temp / 10;
+		}
+		System.out.println("Sum of digits:" +sum);
+	}
+
+}
