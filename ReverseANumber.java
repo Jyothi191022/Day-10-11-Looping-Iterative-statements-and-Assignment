718e59e5c288dfd21640d06ccf599b55266cf2aa
Jyothi Ganapathiraju <96363260+Jyothi191022@users.noreply.github.com>
2025-10-25 14:37:41 +0530

    reverse a number

---
+++ ReverseANumber.java
@@ -0,0 +1,20 @@
+package com.codegnan.controlstatements;
+
+import java.util.Scanner;
+
+public class ReverseANumber {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		int reversed = 0;
+		while (num > 0) {
+			int digit = num % 10;
+			reversed = reversed * 10 + digit;
+			num = num / 10;
+		}
+		System.out.println(reversed);
+		scanner.close();
+	}
+
+}
